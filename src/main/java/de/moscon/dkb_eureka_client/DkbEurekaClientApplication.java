2bf1bbc83f7180d7f2c8d7f628615bc4bd3d1e69
mos <mos.cheele+github@gmail.com>
2021-06-08 11:08:44 +0200

    Configuration Eureka client

--- src/main/java/de/moscon/dkb_eureka_client/DkbEurekaClientApplication.java
+++ src/main/java/de/moscon/dkb_eureka_client/DkbEurekaClientApplication.java
@@ -3,6 +3,10 @@ package de.moscon.dkb_eureka_client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+     https://github.com/spring-guides/gs-service-registration-and-discovery
+ */
+
 @SpringBootApplication
 public class DkbEurekaClientApplication {
 
