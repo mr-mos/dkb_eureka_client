ae76a729c23e0d5ccba6c45202f7c5950d993b0e
mos <mos.cheele+github@gmail.com>
2021-06-09 11:43:38 +0200

    Use LoadBalancer from Feign

--- src/main/java/de/moscon/dkb_eureka_client/VaccinationController.java
+++ src/main/java/de/moscon/dkb_eureka_client/VaccinationController.java
@@ -1,5 +1,6 @@
 package de.moscon.dkb_eureka_client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,15 @@ import java.util.Random;
 @RestController
 public class VaccinationController {
 
+	@Value("${server.port:8080}")
+	private String port;
+
 	@GetMapping("/next-vaccination")
 	public String getNextVaccinationDate(@RequestParam(required = false) String lastname) {
 		if (lastname == null) {
 			return "Bitte gebe deinen Namen als 'lastname'-Parameter an";
 		}
 		LocalDateTime nextDate = LocalDateTime.now().plusDays(new Random().nextInt(200));
-		return String.format("%s, dein Impftermin ist am  %tD", lastname, nextDate);
+		return String.format("%s, dein Impftermin ist am  %tD. [Running on Port %s]", lastname, nextDate, port);
 	}
 }
--- src/main/java/de/moscon/dkb_eureka_client/simulate_another_client/client/MosMicroserviceClient.java
+++ src/main/java/de/moscon/dkb_eureka_client/simulate_another_client/client/MosMicroserviceClient.java
@@ -1,9 +1,12 @@
 package de.moscon.dkb_eureka_client.simulate_another_client.client;
 
+import de.moscon.dkb_eureka_client.lb_config.LoadBalancerConfiguration;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@LoadBalancerClient(name="mos-microservice", configuration = LoadBalancerConfiguration.class)
 @FeignClient(name = "mos-microservice")
 public interface MosMicroserviceClient {
 	@GetMapping("/next-vaccination")
