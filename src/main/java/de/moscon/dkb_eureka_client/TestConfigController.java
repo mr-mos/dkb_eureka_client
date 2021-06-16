fce2c8353d8924c42146d853443a7b163ea704d0
mos <mos.cheele+github@gmail.com>
2021-06-16 16:36:38 +0200

    rename controller

--- src/main/java/de/moscon/dkb_eureka_client/TestConfigController.java
+++ src/main/java/de/moscon/dkb_eureka_client/TestConfigController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class TestConfigController {
 
 	@Value("${my.test.config.entry}")
 	private String myTestConfigEntry;
